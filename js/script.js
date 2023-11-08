77e35daaa6d2596ae8031d22c189fcc239811d3b
natabuz545 <150244658+natabuz545@users.noreply.github.com>
2023-11-08 14:00:17 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $('.clients_talk-slider').slick({
+        slidesToShow: 2,
+        prevArrow:"<img class='slider_left' src='./img/slider_left.png'>",
+            nextArrow:"<img class='slider_right' src='./img/slider_right.png'>",
+
+            responsive: [
+                {
+                  breakpoint: 960,
+                  settings: {
+                    slidesToShow: 1,
+                
+                  }
+                }
+              ]
+
+    });
+
+
+
+    let menuBtn = document.querySelector('.menu-btn');
+    let menu = document.querySelector('.menu');
+    menuBtn.addEventListener('click', function(){
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('active');
+    })
+    
+    console.log(2134);
+  
+
+
+    $(".menbtn .downitem").click(function(){
+
+        console.log(213);
+        $(this).find(".dropdown-content_m").toggleClass("mio43");
+    });
+    
+    
+
+  });
+
+
+
+
+
+
+
